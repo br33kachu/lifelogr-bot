288af3809bc786edb203d8e8c71cfccf2df9dae2
Marco Kretz <mk@marco-kretz.de>
2016-11-23 11:57:05 +0100

    Added bootloader module aka Main-Module

---
+++ bootloader/src/main/java/Bootloader.java
@@ -0,0 +1,10 @@
+import de.lifelogr.communicator.Communicator;
+
+public class Bootloader
+{
+    public static void main(String[] args)
+    {
+        // Init Communicator
+        Communicator c = Communicator.getInstance();
+    }
+}
