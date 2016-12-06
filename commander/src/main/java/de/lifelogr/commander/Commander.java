b3ebfb253f173ec0aa3d6eecd5412b396b0dc59e
Bozhidar Avrenliev <bozhidar.avrenliev@smail.th-koeln.de>
2016-12-06 23:59:27 +0100

    Commander, HelpCommand, StartCommand und TrackCommand klassen erstellt. vollständiges Implementierung folgt in Kurze

--- commander/src/main/java/de/lifelogr/commander/Commander.java
+++ commander/src/main/java/de/lifelogr/commander/Commander.java
@@ -1,7 +1,31 @@
 package de.lifelogr.commander;
 
+import de.lifelogr.commander.Commands.HelpCommand;
+import de.lifelogr.commander.Commands.StartCommand;
+import de.lifelogr.commander.Commands.TrackCommand;
+
 /**
- * Created by bozhidar on 01/12/2016.
+ * Created by bozhidar on 06/12/2016.
  */
 public class Commander {
+    private static Commander instance = null;
+
+    private StartCommand startcmd;
+    private HelpCommand helpcmd;
+    private TrackCommand trackcmd;
+
+    private Commander()
+    {
+        this.startcmd = new StartCommand();
+        this.helpcmd = new HelpCommand();
+        this.trackcmd = new TrackCommand();
+    }
+
+    public static Commander getInstance()
+    {
+        if (instance == null) {
+                    instance = new Commander();
+        }
+        return instance;
+    }
 }
