34cd4b633ffe172595ec286f6296d9590b9d8224
Marco Kretz <mk@marco-kretz.de>
2016-12-06 12:18:25 +0100

    Translator: Implementierung als Singleton

--- translator/src/main/java/de/lifelogr/translator/Translator.java
+++ translator/src/main/java/de/lifelogr/translator/Translator.java
@@ -12,6 +12,33 @@ import de.lifelogr.translator.matcher.TrackCommandMatcher;
  */
 public class Translator
 {
+    public static Translator instance = null;
+    private static Object mutex = new Object();
+
+    private HelpCommandMatcher hcm;
+    private StartCommandMatcher scm;
+    private TrackCommandMatcher tcm;
+
+    private Translator()
+    {
+        this.hcm = new HelpCommandMatcher();
+        this.scm = new StartCommandMatcher();
+        this.tcm = new TrackCommandMatcher();
+    }
+
+    public static Translator getInstance()
+    {
+        if (instance == null) {
+            synchronized (mutex) {
+                if (instance == null) {
+                    instance = new Translator();
+                }
+            }
+        }
+
+        return instance;
+    }
+
     /**
      * Übersetze einen vom User gesendeten Text in das entsprechende Kommando.
      *
@@ -23,18 +50,15 @@ public class Translator
         // Text normalisieren
         text = text.trim().toLowerCase().replaceAll(" +", " ");
 
-        HelpCommandMatcher hcm = new HelpCommandMatcher();
-        if (hcm.matches(text)) {
+        if (this.hcm.matches(text)) {
             return "/help";
         }
 
-        StartCommandMatcher scm = new StartCommandMatcher();
-        if (scm.matches(text)) {
+        if (this.scm.matches(text)) {
             return "/start";
         }
 
-        TrackCommandMatcher tcm = new TrackCommandMatcher();
-        if (tcm.matches(text)) {
+        if (this.tcm.matches(text)) {
             return tcm.getTrackingParams(text).toString();
         }
 
