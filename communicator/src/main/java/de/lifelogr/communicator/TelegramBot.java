069408dea0ecabbac8286561dfad7c93603d8284
Marco Kretz <mk@marco-kretz.de>
2016-12-09 18:07:00 +0100

    Rufe nach dem Übersetzten das passende Kommando auf

--- communicator/src/main/java/de/lifelogr/communicator/TelegramBot.java
+++ communicator/src/main/java/de/lifelogr/communicator/TelegramBot.java
@@ -7,11 +7,12 @@ import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.bots.commands.BotCommand;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.logging.BotLogger;
 
 /**
- * Created by Marco Kretz
+ * @author Marco Kretz
  */
 public class TelegramBot extends TelegramLongPollingCommandBot
 {
@@ -20,11 +21,16 @@ public class TelegramBot extends TelegramLongPollingCommandBot
     private final String botUsername = "syp_lifelog_bot";
     private final String botToken = "289154338:AAHw9d0vFfamnqQjIJDiCpPrXfHcRaG3cwg";
 
+    /**
+     * Konstruktor
+     */
     public TelegramBot()
     {
+        // Registriere Kommando "/help"
         HelpCommand helpCommand = new HelpCommand(this);
         register(helpCommand);
 
+        // Registriere Aktion für unbekanntes Kommando
         registerDefaultAction(((absSender, message) -> {
             SendMessage commandUnknownMessage = new SendMessage();
             commandUnknownMessage.setChatId(message.getChatId().toString());
@@ -34,10 +40,16 @@ public class TelegramBot extends TelegramLongPollingCommandBot
             } catch (TelegramApiException e) {
                 BotLogger.error(LOGTAG, e);
             }
-            helpCommand.execute(absSender, message.getFrom(), message.getChat(), new String[] {});
+            helpCommand.execute(absSender, message.getFrom(), message.getChat(), new String[]{});
         }));
     }
 
+    /**
+     * Verarbeite Nachrichten, die kein Kommando enthalten.
+     * Versuche den Text in ein entsprechendes Kommando umzusetzen und führe dies aus.
+     *
+     * @param update
+     */
     @Override
     public void processNonCommandUpdate(Update update)
     {
@@ -47,14 +59,28 @@ public class TelegramBot extends TelegramLongPollingCommandBot
             if (message.hasText()) {
                 SendMessage sendMessage = new SendMessage();
                 sendMessage.setChatId(message.getChatId().toString());
+
+                // Übersetzte Text in Kommando
                 String translatedCommand = Translator.getInstance().translate(message.getText());
-                if (translatedCommand == null) {
-                    sendMessage.setText("Ich habe keine Ahnung.." + Emoji.CRYING_FACE);
-                } else {
-                    sendMessage.setText(translatedCommand);
+
+                if (translatedCommand != null) {
+                    // Hole das entsprechende Kommando
+                    BotCommand cmd = this.getRegisteredCommand(translatedCommand.split(" ")[0].substring(1));
+
+                    if (cmd != null) {
+                        cmd.execute(
+                                this,
+                                update.getMessage().getFrom(),
+                                update.getMessage().getChat(),
+                                translatedCommand.split(" ")
+                        );
+
+                        return;
+                    }
+
                 }
 
-
+                sendMessage.setText("Sorry, das habe ich nicht verstanden..." + Emoji.DISAPPOINTED_BUT_RELIEVED_FACE);
                 try {
                     sendMessage(sendMessage);
                 } catch (TelegramApiException e) {
