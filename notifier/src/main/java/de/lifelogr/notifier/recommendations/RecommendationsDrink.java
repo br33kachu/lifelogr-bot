e149f03126abe44c151a45343e328094bb4312cd
Christin Schlimbach <cschlimb@gmail.com>
2016-12-13 22:59:55 +0100

    last bug fixes

--- notifier/src/main/java/de/lifelogr/notifier/recommendations/RecommendationsDrink.java
+++ notifier/src/main/java/de/lifelogr/notifier/recommendations/RecommendationsDrink.java
@@ -15,9 +15,11 @@ public class RecommendationsDrink {
         this.recommendations.put(TrackingObjectType.CAFFEIN, new String[]
                 {"Du hast heute schon sehr viel Koffein zu dir genommen! Trink doch lieber mal ein Glas Wasser, das schont deinen Magen.",
                 "Trink doch lieber mal etwas Neutrales. Zu viel Koffein schadet deinem Magen."});
-        this.recommendations.put(TrackingObjectType.ALCOHOL, new String[] {"Du hast schon ziemlich viel Alkohol getrunken. Trink lieber mal etwas langsamer."});
-        this.recommendations.put(TrackingObjectType.BEAUTY, new String[] {"Wow, das ist ein echter Schönmacher!",
-                "Das wirkt echt Wunder für Haut und Haare!",
+        this.recommendations.put(TrackingObjectType.ALCOHOL, new String[] {"Du hast schon ziemlich viel Alkohol getrunken. Mach lieber mal halblang ;)",
+                "Bist du nicht langsam betrunken? Pass lieber ein bisschen auf :D"});
+        this.recommendations.put(TrackingObjectType.BEAUTY, new String[]
+                {"Wow, das ist ein echter Schönmacher!",
+                "Das wirkt wirklich Wunder für Haut und Haare!",
                 "Super! Das macht Haut und Haare gesund und schön."});
     }
 
